bef5468c191b9620809d1641bfedde100e07ee34
AmarokIce <snowlyicewolf@outlook.com>
2025-06-06 18:10:58 +0800

    🐺❓

--- ManorBounty/src/main/java/com/djinfinite/manors_bounty/procedures/DoFruitEffectEnableCheckProcedure.java
+++
@@ -1,9 +0,0 @@
-package com.djinfinite.manors_bounty.procedures;
-
-import com.djinfinite.manors_bounty.configuration.FruitsEffectConfig;
-
-public class DoFruitEffectEnableCheckProcedure {
-	public static boolean execute() {
-		return FruitsEffectConfig.FOOD_EFFECT_ENABLED.get();
-	}
-}
